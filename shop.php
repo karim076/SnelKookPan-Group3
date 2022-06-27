b7f2bb683c0baff9504da26ecaa2c016de3a0f96
Tjeerd Kootwijk <tjeerdgaming@gmail.com>
2022-06-27 09:59:55 +0200

    made shop.php

---
+++ shop.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/shop.css">
+    <title>Shop</title>
+</head>
+<body>
+    <div class="shop_container">
+        <div class="side_bar">
+            <h2>filters</h2>
+        </div>
+        <div class="grid_container">
+            
+            <?php
+                for ($x = 0; $x <= 10; $x++) {
+            ?>
+            <div class="grid_item">
+                <p>prijs</p>
+                <p>groote</p>
+                <p>slaapkamers</p>
+                <p>gasten</p>
+            </div>
+            <?php
+                
+                }
+            ?>
+            
+            
+        </div>
+    </div>
+</body>
+</html>
