2b3563486bd1dd030e654f7922ec2971d614c9e7
Sharmake-Omar <93530529+Sharmake-Omar@users.noreply.github.com>
2022-06-27 09:56:18 +0200

    header gemaakt

---
+++ header.php
@@ -0,0 +1,28 @@
+<?php 	
+	require_once('header.php')
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <title>SnelKookPlan</title>
+</head>
+<body>
+<header>
+		 <nav>
+	        </div>
+            <h1>
+	        <ul class="nav_links">
+			   <li><a href="index.php">Home</a></li>
+			   <li><a href="shop.php">Shop</a></li>
+			   <li><a href="overons.php">Over ons</a></li>
+			   <li><a href="contact.php">Contact</a></li>
+		    </ul>
+		  </nav>
+	</header>
+</body>
+</html>
