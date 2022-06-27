d8872e72e3db2f604eb2a1849de51ef037a85a8c
Koen-Smit <93587490+Koen-Smit@users.noreply.github.com>
2022-06-27 09:56:50 +0200

    index.php + head.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <?php require_once 'head.php'; ?>
+    <title>Home</title>
+</head>
+<body>
+    <?php require_once 'header.php'; ?>
+    
+    <main>
+        
+    </main>
+
+    <?php require_once 'footer.php'; ?>
+</body>
+</html>
