3c6a0b0189e3eaec473f3937ec2ecefbd4de0954
karim076 <59794989+karim076@users.noreply.github.com>
2022-06-28 12:43:51 +0200

    Added login new style intergrated

--- login/login.php
+++ login/login.php
@@ -1,35 +1,56 @@
-<!doctype html>
-<html lang="nl">
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <?php require_once '../head.php'; ?>
+  <?php require_once '../head.php' ; session_start();?>
+  <link rel="stylesheet" href="../css/login.css">
 </head>
-
 <body>
-    <?php require_once '../header.php'; ?>
-    <?php
-    if(isset($_GET['msg']))
-    {
-        echo "<div class='msg'>" .$_GET['msg']. "</div>";
-    }
-    ?>
-    <div class="container home">
-        <form action="backend/loginController.php" method="POST">
-        <div class="form-group">
-            <label for="username">Gebruikersnaam:</label>
-            <input type="text" name="username" id="username">
-        </div>
-        <div class="form-group">
-            <label for="password">Wachtwoord:</label>
-            <input type="password" name="password"id="password">
-        </div>
-
-        <input type="submit" value="Login">
-        </form>
-        
-
+  <header>
+    <?php require_once '../header.php'?>
+  </header>
+  <main>
+	<div class="center"><h2>Inloggen</h2></div>
+	<?php  
+	if(isset($_SESSION['logged'])): ?>
+		<div><?php echo $_SESSION['logged']; ?></div>
+	<?php endif;?>
+	<div class="container">
+		<div class="screen">
+			<div class="screen__content">
+				<form class="login" method="POST" action="../backend/loginController.php">
+					<div class="login__field">
+						<i class="login__icon fas fa-user"></i>
+						<input type="text" class="login__input" name="username" placeholder="Username" required>
+					</div>
+					<div class="login__field">
+						<i class="login__icon fas fa-lock"></i>
+						<input type="password" class="login__input" name="pass" placeholder="Wachtwoord" required>
+					</div>
+					<input type="submit" content="Log In Now" class="button login__submit">
+					</button>				
+				</form>
+				<div class="social-login">
+					<h3>Geen account</h3>
+					<div class="social-icons">
+						<a href="register.php">Klik hier</a>
+					</div>
+				</div>
+			</div>
+			<div class="screen__background">
+				<span class="screen__background__shape screen__background__shape4"></span>
+				<span class="screen__background__shape screen__background__shape3"></span>		
+				<span class="screen__background__shape screen__background__shape2"></span>
+				<span class="screen__background__shape screen__background__shape1"></span>
+			</div>		
+		</div>
+	</div>
+  </main>
+<footer>
+    <div class="wrapper">
+      <div class="copyright">
+        &copy; 2020 - 2021 Karim, Sharmake&nbsp;&nbsp;All Rights Reserved
+      </div>
     </div>
-    <!-- <footer>require_once '../footer.php'?></footer> -->
+  </footer>  
 </body>
-
 </html>
