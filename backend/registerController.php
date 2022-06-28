1865b99e38d9b8f4a0a882970e84dbae9b3feb8e
karim076 <59794989+karim076@users.noreply.github.com>
2022-06-28 13:11:37 +0200

    Added registerController

---
+++ backend/registerController.php
@@ -0,0 +1,66 @@
+<?php
+    session_start();
+    // if(isset($_SESSION['user_id']))
+    // {
+    //     die("Kan niet registreren je bent al ingelogd");    
+    // }
+    $username = $_POST['username'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+    # kijkt of het email geldig is
+    if(filter_var($email,FILTER_VALIDATE_EMAIL) === false)
+    {
+        die('Email is ongeldig!');
+    }
+    # checks
+    if(!isset($username))
+    {
+        $_SESSION['logged'] = "Vul een titel in";
+        header("location: ../userItem/register.php");
+    }
+    else if(empty($name))
+    {
+        $_SESSION['logged'] = "Vul een naam in";
+        header("location: ../userItem/register.php");
+    }
+    else if(empty($email))
+    {
+        $_SESSION['logged'] = "kies een email";
+        header("location: ../userItem/register.php");
+    }
+    else if(empty($pass))
+    {
+        $_SESSION['logged'] = "Vul een wachtwoord in";
+        header("location: ../userItem/register.php");
+    }
+
+    if(isset($errors))
+    {
+        var_dump($errors);
+        die();
+    }
+    // hashing
+    $hash = password_hash($pass, PASSWORD_DEFAULT);
+    //1. Verbinding
+    require_once 'conn.php';
+    
+    //2. Query
+    $query = "INSERT INTO login(username,name,email,pass) 
+              VALUES(:username,:name,:email,:pass)";
+    
+    //3. Prepare
+    $statement=$conn->prepare($query);
+    //4. Execute
+    $statement->execute
+    ([
+        ":name" => $name,
+        ":username" => $username,
+        ":email" => $email,
+        ":pass" => $hash
+    ]);
+    $_SESSION['logged'] = "<div class='msg'>Account succesvol aangemaakt. </div>";
+    $msg = "Uw acount is Succesvol aangemaakt log nu in!";
+    header("location: $base_url/login/login.php?msg=$msg");
+    exit;
+?>
