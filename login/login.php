b2ba9d3450612fd76409d14845e1242b64a5a663
karim076 <59794989+karim076@users.noreply.github.com>
2022-06-28 09:52:59 +0200

    Added login style needed yet

---
+++ login/login.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="nl">
+
+<head>
+    <title>Snelkookpan</title>
+    <?php require_once 'head.php'; ?>
+</head>
+
+<body>
+    <?php require_once 'header.php'; ?>
+    <?php
+    if(isset($_GET['msg']))
+    {
+        echo "<div class='msg'>" .$_GET['msg']. "</div>";
+    }
+    ?>
+    <div class="container home">
+        <form action="backend/loginController.php" method="POST">
+        <div class="form-group">
+            <label for="username">Gebruikersnaam:</label>
+            <input type="text" name="username" id="username">
+        </div>
+        <div class="form-group">
+            <label for="password">Wachtwoord:</label>
+            <input type="password" name="password"id="password">
+        </div>
+
+        <input type="submit" value="Login">
+        </form>
+        
+
+    </div>
+    <footer><?php require_once '../footer.php'?></footer>
+</body>
+
+</html>
