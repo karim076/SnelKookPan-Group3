33587a0d44889fa1e224e31768208de4eaa80e62
Koen-Smit <93587490+Koen-Smit@users.noreply.github.com>
2022-06-28 14:00:15 +0200

    footer links added

--- footer.php
+++ footer.php
@@ -1,5 +1,12 @@
 <footer>
-    <div class="contact">
+    <div class="footerLinkjes">
+        <a href="index.php">Home</a>
+        <a href="shop.php">Vakantie huizen</a>
+        <a href="overons.php">Over ons</a>
+        <a href="contact.php">Contact</a>
+
+    </div>
+    <div class="footerContact">
         <h3>Contact:</h3>
         <p>Tel: 0598-398507</p>
         <p>Mail: fred@snelkookpan.nl</p>
