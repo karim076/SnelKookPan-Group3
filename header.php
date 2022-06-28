4d3fc843ae220378300884e5102a9507f1380584
karim076 <59794989+karim076@users.noreply.github.com>
2022-06-28 09:53:07 +0200

    Added base url

--- header.php
+++ header.php
@@ -1,14 +1,10 @@
-<?php 	
-	require_once('header.php')
-?>
-
 <header>
 	<nav>
 	    <ul class="nav_links">
-			<li><a href="index.php">Home</a></li>
-			<li><a href="shop.php">Shop</a></li>
-			<li><a href="overons.php">Over ons</a></li>
-			<li><a href="contact.php">Contact</a></li>
+			<li><a href="<?php echo $base_url ?>/index.php">Home</a></li>
+			<li><a href="<?php echo $base_url ?>/shop.php">Shop</a></li>
+			<li><a href="<?php echo $base_url ?>/overons.php">Over ons</a></li>
+			<li><a href="<?php echo $base_url ?>/contact.php">Contact</a></li>
 		</ul>
 	</nav>
 </header>
