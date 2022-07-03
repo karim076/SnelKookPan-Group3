241ec505de93c8fdb7558b8b11a4841cf3221129
karim076 <59794989+karim076@users.noreply.github.com>
2022-07-03 15:13:43 +0200

    Added second form in reservern.php

--- reserveren.php
+++ reserveren.php
@@ -10,54 +10,59 @@
     <?php require_once 'header.php'; ?>
     
     <main>
-        <form class="form-style-7">
-            <ul>
-            <li>
-                <label for="name">Aantal personen</label>
-                <input type="number" max="5" name="name" required>
-                <span>Vul het aantal mensen die het huis gaan bezoeken</span>
-            </li>
-            <li>
-                <input type="text" id="datetimerange-input1" size="24" style="text-align:center" min="<?php echo date("d-m-Y") ?>" max="<?php echo date("01-07-2022") ?>" required>
-                <span>Waneer wilt u het huis huren</span>
-                <script>
-                    window.addEventListener("load", function (event) {
-                    new DateRangePicker('datetimerange-input1');
-                });
-        </script>
-            </li>
-            <!-- <li>
-                <script type="text/javascript">
-                var start = document.querySelector('start');
-                start.onclick = function() {
-                $("#end").val(start)
-                }
-                </script>
-                <label for="start">Eind datum:</label>
-                <input type="date" id="end" name="trip-end" value="<?php //echo $show?>" min="<?php //echo date("Y-m-d") ?>" <?php //$Date = date("Y-m-d"); echo date('Y-m-d', strtotime($Date. ' + 1 month'));?>">
-                <span>Einde huur dag</span>
-            </li> -->
-            <li>
-                <label for="email">Email</label>
-                <input type="email" name="email" maxlength="100">
-                <span>Enter a valid email address</span>
-            </li>
-            <li>
-                <label for="url">Website</label>
-                <input type="url" name="url" maxlength="100">
-                <span>Your website address (eg: http://www.google.com)</span>
-            </li>
-            <li>
-                <label for="bio">About You</label>
-                <textarea name="bio" onkeyup="adjust_textarea(this)"></textarea>
-                <span>Say something about yourself</span>
-            </li>
-            <li>
-                <input type="submit" value="Send This" >
-            </li>
-            </ul>
-        </form>
-        
+        <div class="inBetween">
+            <div>
+                <form class="form-style-7">
+                    <label>Reservatie</label>
+                    <ul>
+                        <li>
+                            <label for="gasten">Aantal personen</label>
+                            <input type="number" max="<?php echo $_POST['personen'] ?>" name="gasten" required>
+                            <span>Vul het aantal mensen die het huis gaan bezoeken</span>
+                        </li>
+                        <li>
+                            <label for="datepick">Datum</label>
+                            <input name="datepick" type="text" id="datetimerange-input1" size="24" style="text-align:center" min="<?php echo date("d-m-Y") ?>" max="<?php echo date("01-07-2022") ?>" required>
+                            <span>Waneer wilt u het huis huren</span>
+                            <script>
+                                window.addEventListener("load", function (event) {
+                                new DateRangePicker('datetimerange-input1');
+                            });
+                            </script>
+                        </li>
+                        <li>
+                            <label for="question">Vragen</label>
+                            <!-- <input type="question" name="question" maxlength="100"> -->
+                            <textarea name="question" rows="5" cols="50"></textarea>
+                            <span>Enter a valid email address</span>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+            <div>
+                <form class="form-style-7">
+                    <label>Contact</label>
+                    <ul>
+                        <li>
+                            <label for="Adres">Adress</label>
+                            <input type="number"  name="Adres" required>
+                            <span>Vul hier uw adress in</span>
+                        </li>
+                        <li>
+                            <label for="streetname">Straatnaam</label>
+                            <input name="streetname" type="text" required>
+                            <span>Vul hier uw straatnaam</span>
+                        </li>
+                        <li>
+                            <label for="number">Telefoon nummer</label>
+                            <input type="number" name="question">
+                            <span>Uw telefoon nummer hier invullen</span>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+        </div>
+        <form method="" action="">
     </main>
     <script type="text/javascript">
         //auto expand textarea
