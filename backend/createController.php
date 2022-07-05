4208af38c137a105782408fcd31b9769067e9762
Tjeerd Kootwijk <tjeerdgaming@gmail.com>
2022-07-05 14:06:39 +0200

    Image upload fix

--- backend/createController.php
+++ backend/createController.php
@@ -27,10 +27,35 @@
     {   
         $errors[] = "Vul een aantal Gasten in";
     }
-    $Image = $_POST['Image'];
-    if(empty($Image))
-    {
-        $errors[] = "Vul een Image in";
+    // $Image = $_POST['Image'];
+    // if(empty($Image))
+    // {
+    //     $errors[] = "Vul een Image in";
+    // }
+    $target_dir = "../img/";
+    $target_file = $target_dir . basename($_FILES["Image"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["Image"]["tmp_name"]);
+        if($check !== false) {
+          echo "File is an image - " . $check["mime"] . ".";
+          $uploadOk = 1;
+        } else {
+          echo "File is not an image.";
+          $uploadOk = 0;
+        }
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["Image"]["tmp_name"], $target_file)) {
+        echo "The file ". htmlspecialchars( basename( $_FILES["Image"]["name"])). " has been uploaded.";
+        } else {
+        echo "Sorry, there was an error uploading your file.";
+        }
     }
     //
     
--- huren/create.php
+++ huren/create.php
@@ -8,7 +8,7 @@
     <main>
         <div class="Form_Container">
         <div class="form-style-3">
-            <form action="../backend/createController.php" method="POST">
+            <form action="../backend/createController.php" method="POST" enctype="multipart/form-data">
                 <fieldset><legend>Create</legend>
                 <label for="field1"><span>Omgeving <span class="required">*</span></span><input type="text" class="input-field" name="Omgeving" value="" /></label>
                 <label for="field2"><span>Prijs <span class="required">*</span></span><input type="number" class="input-field" name="Prijs" value="" /></label>
@@ -17,7 +17,7 @@
                 <label for="field6"><span>Gasten <span class="required">*</span></span><input type="number" class="input-field" name="Gasten" value="" /></label>
                 </fieldset>
                 <fieldset><legend>Image</legend>
-                <label for="field6"><span>Image <span class="required">*</span></span><input type="file" name="Image" ></label>
+                <label for="field6"><span>Image <span class="required">*</span></span><input type="file" name="Image" id="Image"></label>
                 <label><span> </span><input type="submit" value="Submit" /></label>
                 </fieldset>
             </form>
