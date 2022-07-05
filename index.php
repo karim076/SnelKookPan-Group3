ca7fd70b92d49eb7fec998d089546843e1dbf292
Koen-Smit <93587490+Koen-Smit@users.noreply.github.com>
2022-07-05 14:01:04 +0200

    HOME PAGE

--- index.php
+++ index.php
@@ -7,7 +7,17 @@
     <?php require_once 'header.php'; ?>
     
     <main>
-        
+        <div class="homepaginaBovenkant">
+            <h1>Snelkookpan</h1>
+        </div>
+        <div class="homefoto">
+            <img src="img\vakantiehuishome.jpeg" alt="homeafbeelding">
+        </div>
+        <div class="homenav">
+            <a href="shop.php">Vakantie huizen</a>
+            <a href="overons.php">Over ons</a>
+            <a href="contact.php">Contact</a>
+        </div>
     </main>
 
     <?php require_once 'footer.php'; ?>
