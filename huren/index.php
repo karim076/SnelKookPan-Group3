8846df2fe977d08f5ad230a2166b0c2c6b3a85ef
Koen-Smit <93587490+Koen-Smit@users.noreply.github.com>
2022-07-05 14:12:40 +0200

    index admin

---
+++ huren/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <?php require_once '../head.php' ?>
+</head>
+<body>
+    <?php require_once '../header.php' ?>
+    <main>
+        
+    </main>
+    <?php require_once '../footer.php' ?>
+</body>
+</html>
