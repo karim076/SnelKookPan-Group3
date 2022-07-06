8990cf725fbce39da0db79ec2e8addcd51c8d75f
Koen-Smit <93587490+Koen-Smit@users.noreply.github.com>
2022-07-06 13:46:53 +0200

    contactpagina

--- contact.php
+++ contact.php
@@ -7,7 +7,32 @@
     <?php require_once 'header.php'; ?>
     
     <main>
-        
+        <div class="contactmain">
+            <div class="extramainc">
+                <div class="contactpagina">
+                    <h3>Contact:</h3>
+                    <p>Tel: 0598-398507</p>
+                    <p>Mail: fred@snelkookpan.nl</p>
+                </div>
+                <div class="openingsuren">
+                    <p class="openings"><strong>Openingsuren telefonisch</strong></p>
+                    <p><strong class="black">Zondag en maandag:</strong>      Gesloten</p>
+                    <p><strong class="black">Dinsdag:</strong>       13u30 tot 18u</p>
+                    <p><strong class="black">Woensdag:</strong>      13u30 tot 18u</p>
+                    <p><strong class="black">Donderdag:</strong>     13u30 tot 18u</p>
+                    <p><strong class="black">Vrijdag:</strong>       9u tot 12u - 13u30 tot 18u</p>
+                    <p><strong class="black">Zaterdag:</strong>      9u tot 12u - 13u tot 16u</p>
+                </div>
+            </div>
+            <div class="mapouter"><div class="gmap_canvas">
+                    <iframe width="425" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=repel%2015%20Zevenbergen&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"> 
+                    </iframe> <a href="http://vin-odometer.info">vin-odometer.info</a>
+                    <style>.mapouter{position:relative;text-align:right;height:500px;width:425px;}</style>
+                    <a href="https://embedmaps.info">Embed Google Maps</a>
+                    <style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:425px;}</style>
+                </div>
+            </div>
+        </div>
     </main>
 
     <?php require_once 'footer.php'; ?>
