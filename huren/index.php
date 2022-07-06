57fc14aae80b871c4a460013875067690d1b3542
Koen-Smit <93587490+Koen-Smit@users.noreply.github.com>
2022-07-06 12:34:46 +0200

    button

--- huren/index.php
+++ huren/index.php
@@ -6,7 +6,9 @@
 <body>
     <?php require_once '../header.php' ?>
     <main>
-        
+        <nav class="adminnav">
+            <a class="adminA" href="create.php">Create</a></a>
+        </nav>
     </main>
     <?php require_once '../footer.php' ?>
 </body>
